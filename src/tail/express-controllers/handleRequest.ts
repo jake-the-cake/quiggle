6948851251f0cd590c5826b49d85370f2bd57c87
jake-the-cake <askjake331@gmail.com>
2023-01-25 14:44:12 -0500

    move functions

--- src/tail/express-controllers/handleRequest.ts
+++ src/tail/express-controllers/handleRequest.ts
@@ -1,24 +1,4 @@
-import { BasicRequest, ResposeObject } from '../../__types__/responseTypes'
-
-const defaultResponseObject: ResposeObject = {
-  success: false,
-  statusCode: 500,
-  errors: null,
-  warnings: null,
-  data: null
-}
-
-export const initResponseObject: BasicRequest = (req, res, next) => {
-  if (!next) return defaultResponseObject
-  req.responseObject = defaultResponseObject
-  next()
-}
-
-export const setDataResponse = ( obj: ResposeObject, statusCode: number): void => {
-  obj.statusCode = statusCode
-  obj.success = true
-  obj.errors = null
-}
+import { BasicRequest } from '../../__types__/responseTypes'
 
 export const getOne: BasicRequest = (req, res) => {
 
--- src/tail/response/index.ts
+++ src/tail/response/index.ts
@@ -1,1 +1,23 @@
+import { BasicRequest, ResposeObject } from "../../__types__/responseTypes"
+
+const defaultResponseObject: ResposeObject = {
+  success: false,
+  statusCode: 500,
+  errors: null,
+  warnings: null,
+  data: null
+}
+
+export const initResponseObject: BasicRequest = (req, res, next) => {
+  if (!next) return defaultResponseObject
+  req.responseObject = defaultResponseObject
+  next()
+}
+
+export const setDataResponse = ( obj: ResposeObject, statusCode: number): void => {
+  obj.statusCode = statusCode
+  obj.success = true
+  obj.errors = null
+}
+
 console.log('tail > response')
