ec2e6d523a21cfd55ef4d26782ea4e8814875d2c
jake-the-cake <askjake331@gmail.com>
2023-01-26 08:45:59 -0500

    minor adjustments

--- src/__tests__/responseObject.test.ts
+++ src/__tests__/responseObject.test.ts
@@ -2,7 +2,7 @@ import { initResponseObject, setDataResponse, setErrorResponse } from '../tail/r
 
 // Testing that the response object sets to the defaults
 const responseObject = initResponseObject({}, null)
-describe('Create a response object', function () {
+describe('Create a response object', function() {
   test('Default success to false', () => expect(responseObject?.success).toBe(false))
   test('Default status of 500', () => expect(responseObject?.statusCode).toBe(500))
   test('Default errors to null', () => expect(responseObject?.errors).toBe(null))
