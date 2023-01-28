0b350cde4cdeb8e2c584508a109ddbc49d9efb7f
jake-the-cake <askjake331@gmail.com>
2023-01-28 08:52:10 -0500

    color object

---
+++ src/tail/logger/defaults.ts
@@ -0,0 +1,39 @@
+type ColorObjectProps = {
+    prefix: string;
+    suffix: string;
+    reset: string;
+    color: {
+        black: string;
+        red: string;
+        green: string;
+        yellow: string;
+        blue: string;
+        magenta: string;
+        cyan: string;
+        white: string;
+    };
+    layer: {
+        font: string;
+        bg: string;
+    };
+}
+
+export const colorObject: ColorObjectProps = {
+  prefix: '\033[',
+  suffix: 'm',
+  reset: '\033[0m',
+  color: {
+    black: '0',
+    red: '1',
+    green: '2',
+    yellow: '3',
+    blue: '4',
+    magenta: '5',
+    cyan: '6',
+    white: '7'
+  },
+  layer: {
+    font: '3',
+    bg: '4'
+  }
+}
